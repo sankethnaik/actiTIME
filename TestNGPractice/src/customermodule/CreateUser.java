ef576c87c71c06cf5186cb5a897e58ec51cd4fce
sanke <sanke@Sanketh>
2025-05-13 13:16:21 +0530

    First modification.

---
+++ TestNGPractice/src/customermodule/CreateUser.java
@@ -0,0 +1,13 @@
+package customermodule;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class CreateUser {
+	
+	@Test
+	public void createUser() {
+		Reporter.log("createUser",true);
+	}
+
+}
